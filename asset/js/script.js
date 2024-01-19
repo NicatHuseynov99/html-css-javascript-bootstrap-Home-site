f1ac3e8b68778d9ee1f2c84f2941a3b5656fd0fb
Nicat Huseynov <nicat.huseynov.m@gmail.com>
2024-01-20 03:54:01 +0400

    the site is created and section post is created and section header-bottom is changed

--- asset/js/script.js
+++ asset/js/script.js
@@ -20,3 +20,66 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
 }
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 2,
+      nav: true,
+    },
+    992: {
+      items: 3,
+      nav: false,
+    },
+    1024: {
+      items: 4,
+      nav: false,
+    },
+    1194: {
+      items: 5,
+      nav: true,
+      loop: true,
+    },
+  },
+});
+
+function clickmenu() {
+  var menu = document.getElementById("categories-menu");
+  var icon = document.getElementById("icon-menu");
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    icon.classList.remove("fa-chevron-up");
+    icon.classList.add("fa-chevron-down");
+  } else {
+    menu.classList.add("active");
+    icon.classList.remove("fa-chevron-down");
+    icon.classList.add("fa-chevron-up");
+  }
+}
+
+function clickmenu2() {
+  var menu = document.getElementById("categories-menu2");
+  var icon = document.getElementById("icon-menu2");
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    icon.classList.remove("fa-chevron-up");
+    icon.classList.add("fa-chevron-down");
+  } else {
+    menu.classList.add("active");
+    icon.classList.remove("fa-chevron-down");
+    icon.classList.add("fa-chevron-up");
+  }
+}
+
+
+function openSidebar(){
+var openSidebar=document.getElementById("sidebar")
+openSidebar.classList.add("active");
+}
+function closeSidebar(){
+  var openSidebar=document.getElementById("sidebar")
+  openSidebar.classList.remove("active");
+  }
